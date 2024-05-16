0fdc9f588b855a0255c29580952f0bb47fcf7ef9
21acad <162068259+kb2love@users.noreply.github.com>
2024-05-16 22:23:51 +0900

    ResourceReduce

--- Assets/01.Scripts/Step01.cs
+++ Assets/01.Scripts/Step01.cs
@@ -8,182 +8,178 @@ public class Step01 : MonoBehaviour
 {
     [Header("AudioSource")]
     [SerializeField] AudioSource teacherSource;     // 선생님의 지시를 플레이할 오디오 소스
-
     [SerializeField] AudioSource stepAudioSource;   // 나레이션 오디오 소스
-
     [SerializeField] AudioSource warningAudioSource;    //화재 경고음이 울릴 오디오 소스
 
     [Header("AudioClip")]
-
     [SerializeField] AudioClip narration;           // 나레이션 오디오 클립
-
     [SerializeField] AudioClip teacherGuid01;       // 선생님의 지시 오디오 클립 01
-
     [SerializeField] AudioClip teacherGuid02;
-
     [SerializeField] AudioClip teacherGuid03;
-
     [SerializeField] AudioClip fireWarningClip;         // 화재경고음 클립
-
     [SerializeField] AudioClip fireWarningClipChange;   // 화재 경고음을 반복시키기 위한 클립 
-
     [SerializeField] AudioClip clikClip;            // 클릭 오디오 클립
 
-
     [Header("Animator")]
-
     [SerializeField] Animator teacherAni;           // 선생님의 애니메이터
-
     [SerializeField] Animator[] kidAnimations;
 
     [Header("Transform")]
-
     [SerializeField] Transform playerTr;              // 플레이어를 이동시킬 트랜스폼
-
     [SerializeField] Transform teacherTr;             // 선생님을 이동시킬 트랜스폼
-
     [SerializeField] Transform[] kidsTr;
-
     [SerializeField] Transform[] targetTr;      // 이동할 위치
 
     [SerializeField] GameObject nextStep;           // 다음 스탭으로 넘어가기 위한 오브젝트
-
     [SerializeField] GameObject fire;
 
     AudioManager audioManager;                  // 오디오 매니저를 사용하기 위한 변수
-
     TweenManager tweenManager;                  // 트윈 매니저를 사용하기 위한 변수
+
+    float startVal = 0.0f;
+    float endVal = 1.0f;
+
     void OnEnable()
     {
-        tweenManager = new TweenManager();                      // 모노 비헤이버를 상속받지 않아도 되기 때문에 클라스를 생성 시켜 준다
+        tweenManager = new TweenManager();
         tweenManager.PopUpUI(transform);
 
-        audioManager = gameObject.AddComponent<AudioManager>(); // 네임 스페이스로 제작 해 놓은 클래스(모노비헤이버상속)
-        audioManager.NarrationPlay(stepAudioSource, narration, 1.0f);      //생성된 오디오 매니저 클라스의 나레이션 메서드를 사용 할 수 있다
+        audioManager = gameObject.AddComponent<AudioManager>();
+        audioManager.NarrationPlay(stepAudioSource, narration, 1.0f);
 
         targetTr = GameObject.Find("TargetPosition").GetComponentsInChildren<Transform>();
 
-        teacherAni.SetTrigger("TalkTrigger");
+        teacherAni.SetBool("IsTalk", true);
     }
+
     void OnDisable()
     {
-        tweenManager = null;                                // 생성했던 트윈 매니저를 비워준다
-        audioManager = null;                                // 생성했던 오디오 매니저를 지워준다
-        Destroy(gameObject.GetComponent<AudioManager>());   // 추가했던 콤포넌트를 삭제 해 준다
+        tweenManager = null;
+        audioManager = null;
+        Destroy(gameObject.GetComponent<AudioManager>());
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "LeftCol" || other.gameObject.tag == "RightCol")
         {
             OnClickMethods();
-
         }
     }
+
     public void OnClickMethods()
     {
-        teacherSource.PlayOneShot(clikClip, 1.0f);            // 중복이 되는 단발성 오디오 플레이
-        Sequence seq = DOTween.Sequence();
-        seq.AppendCallback(() => tweenManager.CloseUI(transform));  //UI를 닫는다
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
-            Debug.Log("asd");
-            audioManager.LoopAudioPlay(warningAudioSource, fireWarningClip);  // 화재 경고음 재생
-            teacherAni.SetTrigger("LookAround");            // 선생님이 불이 난걸 알아챈다
-            fire.SetActive(true);                           // 불을 켜준다
-            for (int i = 0; i < kidAnimations.Length; i++)
-            {
-                kidAnimations[i].SetTrigger("StandUp");     // 아이들이 일어난다
-                kidsTr[i].DOMoveZ(1.11f, 1.0f);             // 일어났을때 의자에 떨어지도록 약간 움직여준다
-            }
-        });
-        seq.AppendInterval(2.0f);
-        seq.AppendCallback(() => audioManager.LoopAudioPlay(warningAudioSource, fireWarningClipChange));  //화재경고음의 시작부분을 바꿀 시퀀스
-        seq.AppendInterval(4.0f);
-        seq.AppendCallback(() =>
-        {
-            // 대화 애니메이션 실행 및 선생님의 지시 오디오 재생
-            teacherAni.SetTrigger("TalkTrigger");
-            // 선생님의 지시 오디오를 재생합니다.
-            teacherSource.PlayOneShot(teacherGuid01, 1.0f);
-        });
-        seq.AppendInterval(7.0f);
-        seq.AppendCallback(() => teacherSource.PlayOneShot(teacherGuid02, 1.0f));       //선생님의 2번째 지시 오디오 실행
-        seq.AppendInterval(7.0f);
-        seq.AppendCallback(() => CharacterMove());              //캐릭터들의 움직임을 실행한다
+        teacherSource.PlayOneShot(clikClip, 1.0f);
 
+        Sequence seq = DOTween.Sequence();
+        seq.AppendCallback(() => tweenManager.CloseUI(transform));
+        seq.AppendInterval(1.0f);
+        seq.AppendCallback(HandleFireWarning);
+        seq.AppendInterval(2.0f);
+        seq.AppendCallback(() => audioManager.LoopAudioPlay(warningAudioSource, fireWarningClipChange));
+        seq.AppendInterval(4.0f);
+        seq.AppendCallback(() => PlayTeacherGuidance(teacherGuid01, "IsTalk"));
+        seq.AppendInterval(7.0f);
+        seq.AppendCallback(() => teacherSource.PlayOneShot(teacherGuid02, 1.0f));
+        seq.AppendInterval(7.0f);
+        seq.AppendCallback(CharacterMove);
+    }
+
+    private void HandleFireWarning()
+    {
+        audioManager.LoopAudioPlay(warningAudioSource, fireWarningClip);
+        teacherAni.SetTrigger("LookAround");
+        fire.SetActive(true);
+
+        foreach (var kidAnimation in kidAnimations)
+        {
+            kidAnimation.SetTrigger("StandUp");
+        }
+
+        foreach (var kidTr in kidsTr)
+        {
+            kidTr.DOMoveZ(1.11f, 1.0f);
+        }
     }
 
     public void CharacterMove()
     {
         Sequence seq = DOTween.Sequence();
-        float startVal = 0.0f;
-        float endVal = 1.0f;
 
-        seq.AppendCallback(() =>
-        {   
-            teacherAni.SetTrigger("moveTrigger");   // 선생님의 무브블랜드를 켜준다
-            Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)  // startVal을 1로 만드는걸 DOTween.To 를써서 보간시켜 1로 만들어주는 트윈
-            .OnUpdate(() => teacherAni.SetFloat("moveSpeed", startVal));
-
-            Sequence seq01 = DOTween.Sequence();    // 새로운 시퀀스를 만든다
-            seq01.Append(floatTween);               // 보간을 넣어준다
-            seq01.Join(teacherTr.DORotate(new Vector3(0f, 90f, 0f), 0.5f));     // 동시에 움직인다
-            seq01.Join(teacherTr.DOMove(targetTr[4].position, 5.0f));  // 동시에 회전한다
-        });
+        seq.AppendCallback(() => MoveTeacher());
         seq.AppendInterval(4.0f);
-        seq.AppendCallback(() =>
-        {
-            endVal = 0.0f;  //endVal 을다시 0으로 초기화시켜 선생님이 걷는걸 멈추게한다
-            Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)
-            .OnUpdate(() => teacherAni.SetFloat("moveSpeed", startVal));
-        });
+        seq.AppendCallback(() => StopCharacter());
         seq.AppendInterval(1.0f);
         seq.Append(teacherTr.DORotate(new Vector3(0f, -90f, 0f), 1.5f));
-        seq.AppendCallback(() =>
-        {
-            teacherAni.SetTrigger("TalkTrigger");           // 줄을 서라는 선생님의 지시 애니메이션 실행
-            teacherSource.PlayOneShot(teacherGuid03, 1.0f); // 줄을 서라는 선생님의 지시 오디오 실행
-        });
+        seq.AppendCallback(() => PlayTeacherGuidance(teacherGuid03, "IsTalk"));
         seq.AppendInterval(7.0f);
-        seq.AppendCallback(() =>
-        {
-            teacherAni.SetTrigger("moveTrigger");           // 말이끝나면 다시 Idle 상태로 바꿔준다
-            endVal = 1.0f;                                  // 아이들의 움직임을 위해 endVal = 1.0으로 초기화 한다
-            Sequence seq01 = DOTween.Sequence(); 
+        seq.AppendCallback(() => StartKidsMove(startVal, endVal));
+    }
 
-            Tween lerpTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f)
-                .SetUpdate(true)
-                .OnUpdate(() =>
-                {
-                    kidAnimations[0].SetFloat("moveSpeed", startVal);
-                    kidAnimations[1].SetFloat("moveSpeed", startVal);
-                    kidAnimations[2].SetFloat("moveSpeed", startVal);
+    private void MoveTeacher()
+    {
+        teacherAni.SetBool("IsTalk", false);
 
-                });
+        Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)
+            .OnUpdate(() => teacherAni.SetFloat("moveSpeed", startVal));
+        Sequence moveSeq = DOTween.Sequence();
+        moveSeq.Append(floatTween);
+        moveSeq.Join(teacherTr.DORotate(new Vector3(0f, 90f, 0f), 1.0f));
+        moveSeq.Join(teacherTr.DOMove(targetTr[4].position, 5.0f));
+    }
 
-            seq01.Append(lerpTween);
-            seq01.Join(kidsTr[0].DORotate(new Vector3(0f, 90f, 0f), 5.0f));
-            seq01.Join(kidsTr[1].DORotate(new Vector3(0f, 90f, 0f), 5.0f));
-            seq01.Join(kidsTr[2].DORotate(new Vector3(0f, 90f, 0f), 5.0f));
+    private void StopCharacter()
+    {
+        endVal = 0.0f;
+        Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)
+            .OnUpdate(() => teacherAni.SetFloat("moveSpeed", startVal));
+    }
 
-            seq01.Join(kidsTr[0].DOMove(targetTr[3].position, 5.0f));
-            seq01.Join(kidsTr[1].DOMove(targetTr[2].position, 5.0f));
-            seq01.Join(kidsTr[2].DOMove(targetTr[1].position, 5.0f));
-            seq01.AppendCallback(() =>
+
+    private void PlayTeacherGuidance(AudioClip clip, string _bool)
+    {
+        teacherAni.SetBool(_bool, true);
+        teacherSource.PlayOneShot(clip, 1.0f);
+    }
+    private void StartKidsMove(float startVal, float endVal)
+    {
+        endVal = 1.0f;
+
+        Sequence kidsSeq = DOTween.Sequence();
+
+        Tween lerpTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f)
+            .SetUpdate(true)
+            .OnUpdate(() =>
             {
-                endVal = 0.0f;
-                Tween lerpTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f)
-                .SetUpdate(true)
-                .OnUpdate(() =>
+                foreach (var kidAnimation in kidAnimations)
                 {
-                    kidAnimations[0].SetFloat("moveSpeed", startVal);
-                    kidAnimations[1].SetFloat("moveSpeed", startVal);
-                    kidAnimations[2].SetFloat("moveSpeed", startVal);
-
-                });
+                    kidAnimation.SetFloat("moveSpeed", startVal);
+                }
             });
-            seq01.AppendCallback(() => tweenManager.CloseUI(transform, nextStep, 1.0f));    // 창을 닫고 다음 스텝을 여는 트윈 메서드 실행
-        });
+
+        kidsSeq.Append(lerpTween);
+
+        for (int i = 0; i < kidsTr.Length; i++)
+        {
+            kidsSeq.Join(kidsTr[i].DORotate(new Vector3(0f, 90f, 0f), 5.0f));
+            kidsSeq.Join(kidsTr[i].DOMove(targetTr[3 - i].position, 5.0f));
+        }
+
+        kidsSeq.AppendCallback(() => StopKidsMove(startVal, endVal));
+        kidsSeq.AppendCallback(() => tweenManager.CloseUI(transform, nextStep, 1.0f));
+    }
+
+    private void StopKidsMove(float startVal, float endVal)
+    {
+        endVal = 0.0f;
+        Tween lerpTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f)
+            .SetUpdate(true)
+            .OnUpdate(() =>
+            {
+                foreach (var kidAnimation in kidAnimations)
+                {
+                    kidAnimation.SetFloat("moveSpeed", startVal);
+                }
+            });
     }
 }
--- Assets/01.Scripts/Step03.cs
+++ Assets/01.Scripts/Step03.cs
@@ -1,75 +1,59 @@
 using DG.Tweening;
 using UnityEngine;
 using VrFireSim;
-
 public class Step03 : MonoBehaviour
 {
     [Header("AudioSource")]
-
     [SerializeField] AudioSource stepSource;   // 나레이션을 플레이할 오디오 소스
-
     [SerializeField] AudioSource[] kidsSource;
-
     [SerializeField] AudioSource teacherSource;
 
     [Header("AudioClip")]
-
     [SerializeField] AudioClip clikClip;        // 클릭 오디오 클립
-
     [SerializeField] AudioClip narration03;     // 나레이션 오디오 클립
-
     [SerializeField] AudioClip teacherGuid04;   // 선생님의 네번째 지시 클립
-
     [SerializeField] AudioClip coughtClip01;    // 기침하는 오디오 클립
-
     [SerializeField] AudioClip coughtClip02;
-
     [SerializeField] AudioClip coughtClip03;
 
     [Header("Animator")]
-
     [SerializeField] Animator[] kidsAni;
-
     [SerializeField] Animator teacherAni;
 
     [Header("Transform")]
-
     [SerializeField] Transform[] kidsTr;    //애들의 위치를 리스트로 가져옴
+    [SerializeField] Transform[] targetTr;  // 이동할 위치
+    [SerializeField] Transform teacherTr;   // 선생님의 위치
+    [SerializeField] Transform playerTr;    // 플레이어의 위치
+    [SerializeField] GameObject nextStep;   // 다음 스탭으로 넘어가기 위한 오브젝트
 
-    [SerializeField] Transform[] targetTr;      // 이동할 위치
-
-    [SerializeField] Transform teacherTr;       // 선생님의 위치
-
-    [SerializeField] Transform playerTr;        // 플레이어의 위치
-
-    [SerializeField] GameObject nextStep;       // 다음 스탭으로 넘어가기 위한 오브젝트
-    AudioManager audioManager;                  // 오디오 매니저를 사용하기 위한 변수
-
-    TweenManager tweenManager;                  // 트윈 매니저를 사용하기 위한 변수
-
+    AudioManager audioManager;              // 오디오 매니저를 사용하기 위한 변수
+    TweenManager tweenManager;              // 트윈 매니저를 사용하기 위한 변수
 
     float startVal = 0.0f;
-
     float endVal = 1.0f;
+    float timeTp = 0.5f;
     void OnEnable()
     {
-        tweenManager = new TweenManager();                  // 모노 비헤이버를 상속받지 않아도 되기 때문에 클라스를 생성 시켜 준다
+        tweenManager = new TweenManager();
         tweenManager.PopUpUI(transform);
 
-        audioManager = gameObject.AddComponent<AudioManager>();                        // 네임 스페이스로 제작 해 놓은 클래스(모노비헤이버상속)
-        audioManager.NarrationPlay(stepSource, narration03, 1.0f);                    //생성된 오디오 매니저 클라스의 나레이션 메서드를 사용 할 수 있다
+        audioManager = gameObject.AddComponent<AudioManager>();
+        audioManager.NarrationPlay(stepSource, narration03, 1.0f);
 
-        targetTr = GameObject.Find("TargetPosition").GetComponentsInChildren<Transform>();  
+        targetTr = GameObject.Find("TargetPosition").GetComponentsInChildren<Transform>();
     }
+
     void OnDisable()
     {
-        tweenManager = null;                                // 생성했던 트윈 매니저를 비워준다
-        audioManager = null;                                // 생성했던 오디오 매니저를 지워준다
-        Destroy(gameObject.GetComponent<AudioManager>());   // 추가했던 콤포넌트를 삭제 해 준다
+        tweenManager = null;
+        audioManager = null;
+        Destroy(gameObject.GetComponent<AudioManager>());
     }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "LeftCol" || other.gameObject.tag == "RightCol")
+        if (other.gameObject.CompareTag("LeftCol") || other.gameObject.CompareTag("RightCol"))
         {
             OnClickMethods();
         }
@@ -77,98 +61,94 @@ public class Step03 : MonoBehaviour
 
     public void OnClickMethods()
     {
-        stepSource.PlayOneShot(clikClip, 1.0f);            // 중복이 되는 단발성 오디오 플레이
+        stepSource.PlayOneShot(clikClip, 1.0f);
         tweenManager.CloseUI(transform);
-        Vector3 rot = new Vector3(0, 0, 0);
-        Sequence seq = DOTween.Sequence();
 
-        //tweenManager.CloseUI(transform, nextStep, 1.0f);    // 창을 닫고 다음 스텝을 여는 트윈 메서드 실행
-        seq.AppendInterval(0.5f);
-        seq.AppendCallback(() =>
-        {
-            teacherAni.SetTrigger("TalkTrigger");
-            teacherSource.PlayOneShot(teacherGuid04, 1.0f);
-        });
-        seq.AppendInterval(4.0f);
-        charactermove();
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(0.5f)
+           .AppendCallback(() =>
+           {
+               teacherAni.SetTrigger("TalkTrigger");
+               teacherSource.PlayOneShot(teacherGuid04, 1.0f);
+           })
+           .AppendInterval(4.0f)
+           .AppendCallback(() => CharacterMove());
     }
 
-    private void charactermove()
+    private void CharacterMove()
     {
         Sequence seq = DOTween.Sequence();
-        seq.Append(teacherTr.DORotate(new Vector3(0, 90f, 0), 1.5f));
-        seq.AppendCallback(() =>
-        {
-            teacherAni.SetTrigger("moveTrigger");
-            Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)  // startVal을 1로 만드는걸 DOTween.To 를써서 보간시켜 1로 만들어주는 트윈
+
+        seq.Append(teacherTr.DORotate(new Vector3(0, 90f, 0), 1.5f))
+           .AppendCallback(() => StartMove())
+           .AppendInterval(timeTp)
+           .AppendCallback(() => MoveCharacters(targetTr[7], targetTr[6], targetTr[5], targetTr[4], targetTr[3]))
+           .AppendInterval(timeTp)
+           .AppendCallback(() => SeconMove())
+           .AppendInterval(timeTp)
+           .AppendCallback(() => ThirdMove())
+           .AppendInterval(timeTp)
+           .AppendCallback(() => FinalMove())
+           .AppendInterval(timeTp)
+           .AppendCallback(() => EndMove());
+    }
+
+    private void StartMove()            
+    {
+        teacherAni.SetTrigger("moveTrigger");
+        Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)
             .OnUpdate(() =>
             {
                 teacherAni.SetFloat("moveSpeed", startVal);
-                kidsAni[0].SetFloat("moveSpeed", startVal);
-                kidsAni[1].SetFloat("moveSpeed", startVal);
-                kidsAni[2].SetFloat("moveSpeed", startVal);
+                foreach (var kidAni in kidsAni)
+                {
+                    kidAni.SetFloat("moveSpeed", startVal);
+                }
             });
-            teacherTr.DOMoveX(6.5f, 1.0f);
-            kidsTr[0].DOMoveX(5.0f, 1.0f);
-            kidsTr[1].DOMoveX(3.5f, 1.0f);
-            kidsTr[2].DOMoveX(2.0f, 1.0f);
-            playerTr.DOMoveX(0.5f, 1.0f);
 
-            teacherTr.DOMove(targetTr[6].position ,1.0f);   
-            kidsTr[0].DOMove(targetTr[5].position, 1.0f);
-            kidsTr[1].DOMove(targetTr[4].position, 1.0f);
-            kidsTr[2].DOMove(targetTr[3].position, 1.0f);
-            playerTr.DOMove(targetTr[2].position, 1.0f);
-        });
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
-            teacherTr.DORotate(Vector3.zero, 1.0f);
-            teacherTr.DOMove(targetTr[7].position, 1.0f);
-            kidsTr[0].DOMove(targetTr[6].position, 1.0f);
-            kidsTr[1].DOMove(targetTr[5].position, 1.0f);
-            kidsTr[2].DOMove(targetTr[4].position, 1.0f);
-            playerTr.DOMove(targetTr[3].position, 1.0f);
-        });
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
-            kidsTr[0].DORotate(Vector3.zero, 1.0f);
-            kidsTr[0].DOMove(targetTr[8].position, 1.0f);
-            kidsTr[1].DOMove(targetTr[6].position, 1.0f);
-            kidsTr[2].DOMove(targetTr[5].position, 1.0f);
-            playerTr.DOMove(targetTr[4].position, 1.0f);
-        });
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
+        MoveCharacters(targetTr[6], targetTr[5], targetTr[4], targetTr[3], targetTr[2]);
+    }
+    private void SeconMove()
+    {
+        kidsTr[0].DOMove(targetTr[8].position, 1.0f);
+        kidsTr[1].DOMove(targetTr[6].position, 1.0f);
+        kidsTr[2].DOMove(targetTr[5].position, 1.0f);
+        playerTr.DOMove(targetTr[4].position, 1.0f);
+    }
+    private void MoveCharacters(Transform teacherTarget, Transform kid1Target, Transform kid2Target, Transform kid3Target, Transform playerTarget)
+    {
+        teacherTr.DOMove(teacherTarget.position, 1.0f);
+        kidsTr[0].DOMove(kid1Target.position, 1.0f);
+        kidsTr[1].DOMove(kid2Target.position, 1.0f);
+        kidsTr[2].DOMove(kid3Target.position, 1.0f);
+        playerTr.DOMove(playerTarget.position, 1.0f);
+    }
 
-            kidsTr[1].DORotate(Vector3.zero, 1.0f);
-            kidsTr[1].DOMove(targetTr[9].position, 1.0f);
-            kidsTr[2].DOMove(targetTr[6].position, 1.0f);
-            playerTr.DOMove(targetTr[5].position, 1.0f);
-        });
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
-            kidsTr[2].DORotate(Vector3.zero, 1.0f);
-            kidsTr[2].DOMove(targetTr[10].position, 1.0f);
-            playerTr.DOMove(targetTr[6].position, 1.0f);
-        });
-        
-        seq.AppendInterval(1.0f);
-        seq.AppendCallback(() =>
-        {
-            playerTr.DOMove(targetTr[11].position, 1.0f);
-            endVal = 0.0f;
-            Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)  // startVal을 1로 만드는걸 DOTween.To 를써서 보간시켜 1로 만들어주는 트윈
+    private void ThirdMove()
+    {
+        kidsTr[1].DOMove(targetTr[9].position, 1.0f);
+        kidsTr[2].DOMove(targetTr[6].position, 1.0f);
+        playerTr.DOMove(targetTr[5].position, 1.0f);
+    }
+    private void FinalMove()
+    {
+        teacherTr.DORotate(Vector3.zero, 1.0f);
+        kidsTr[2].DOMove(targetTr[10].position, 1.0f);
+        playerTr.DOMove(targetTr[6].position, 1.0f);
+    }
+
+    private void EndMove()
+    {
+        playerTr.DOMove(targetTr[11].position, 1.0f);
+        endVal = 0.0f;
+        Tween floatTween = DOTween.To(() => startVal, x => startVal = x, endVal, 1.0f).SetUpdate(true)
             .OnUpdate(() =>
             {
                 teacherAni.SetFloat("moveSpeed", startVal);
-                kidsAni[0].SetFloat("moveSpeed", startVal);
-                kidsAni[1].SetFloat("moveSpeed", startVal);
-                kidsAni[2].SetFloat("moveSpeed", startVal);
+                foreach (var kidAni in kidsAni)
+                {
+                    kidAni.SetFloat("moveSpeed", startVal);
+                }
             });
-        });
     }
 }
